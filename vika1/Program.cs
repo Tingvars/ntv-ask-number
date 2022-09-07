a63c351ce8024de9729dba5c4fcc939937063eac
Tingvars <89653926+Tingvars@users.noreply.github.com>
2022-09-07 15:48:03 +0000

    first commit

---
+++ vika1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace vika1 
+{ 
+internal class Program
+{
+        private static void Main(string[] args)
+        {
+            string enterednum = "0";
+            List<int> intList = new List<int>();
+            int biggest = 0;
+            int smallest = 0;
+            int enteredint;
+            while (enterednum != "")
+            { 
+        Console.WriteLine("Please enter a number: ");
+                enterednum = Console.ReadLine();
+                if (enterednum != "") {
+                    if (int.TryParse(enterednum, out enteredint) == true) { 
+                    intList.Add(enteredint);
+                    biggest = intList.Max();
+                    smallest = intList.Min();
+                    Console.WriteLine($"You entered {enterednum}");
+                    }
+                }
+            };
+            Console.WriteLine($"Biggest number entered is {biggest} and smallest number entered is {smallest}");
+        }
+}
+}
